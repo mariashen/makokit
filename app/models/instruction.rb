5a65b49f96d640de5eac1499fa43b8f5716354cd
JeanCharles <jc.gasche@mac.com>
2016-04-03 21:53:13 +0300

    fixed bugs in link chain

--- app/models/instruction.rb
+++ app/models/instruction.rb
@@ -5,8 +5,11 @@ class Instruction < ActiveRecord::Base
 	has_many :arriving_answers, through: :answer_jumps, class_name: 'Answer'
 	#link list management
 	attr_accessor :previous_instruction_id
-	before_create { self.previous_instruction_id = self.lesson.instructions.last.id }
-	after_create { Instruction.find(self.previous_instruction_id).update_attribute(:next_instruction_id, self.id) }
+	before_create { self.previous_instruction_id = self.lesson.instructions.last.id if self.lesson.instructions.any? }
+	after_create do 
+		inst= Instruction.find_by_id(self.previous_instruction_id)
+		inst.update_attribute(:next_instruction_id, self.id) if inst
+	end
 	after_destroy do  
 		inst = Instruction.find_by_next_instruction_id(self.id)
 		inst.update_attribute(:next_instruction_id, self.next_instruction_id) if inst 
