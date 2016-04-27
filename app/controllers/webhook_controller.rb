5c396ab7d288d70dee3b50ac43089199a6719e93
Muhammad Khadafi <mk2333@cornell.edu>
2016-04-27 04:44:45 -0400

    webhook request test

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -1,5 +1,7 @@
 class WebhookController < ApplicationController
 
+	require 'unirest'
+	require 'JSON'
 
 	def permission
 		if params['hub.verify_token'] == 'verify_mako'
@@ -10,11 +12,28 @@ class WebhookController < ApplicationController
 	end
 
 	def receive
+		request_body = JSON.parse(request.body.read)
+		messaging_events = request_body.entry[0].messaging
+		for i in 0..messaging_events.count
+			event = request_body.entry[0].messaging[i]
+			sender = event.sender.id
+			if event.message and event.message.text
+				sendTextMessage(sender, "hello")
+			end
+		end
+
+		render status: :ok
 	end
 	
-	# private
+	private
 
-	# 	def instruction_params
-	# 		params.require(:instruction).permit(:lesson_id, :text, :image_url, :video_url, :display_index)
-	# 	end
+		def sendTextMessage(sender, text)
+			token = 'EAADrxm348aEBAIyMoDh1rf3lScB2NCOWWm9IUx9H1AAZB9nWyEFwV5vJ98ejHeJC0Mcpp3DhZCS5yFvAiYU3qwmXXMh3lDt2QaFAr43Tik3ybHhooF3d8WFw97loxWyn9C4Bg0XOJecsrHhAAoHv5IJAcKms5y6fMtzrtiWgZDZD'
+			url = 'https://graph.facebook.com/v2.6/me/messages?access_token=' + token
+			recipientData = {:id => sender}.to_json
+			messageData = {:text => text}.to_json
+			response = Unirest.post url, 
+                        headers:{ "Accept" => "application/json" }, 
+                        parameters:{ :recipient => recipientData, :message => messageData }.to_json
+		end
 end
