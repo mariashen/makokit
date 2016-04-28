612ebb94a84a54882b7bc41f2db1909b06fcb8ae
Muhammad Khadafi <mk2333@cornell.edu>
2016-04-27 22:57:00 -0400

    trying lesson selection

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -4,6 +4,8 @@ class WebhookController < ApplicationController
 	require 'json'
 
 	skip_before_action :verify_authenticity_token
+	token = 'EAADrxm348aEBAIyMoDh1rf3lScB2NCOWWm9IUx9H1AAZB9nWyEFwV5vJ98ejHeJC0Mcpp3DhZCS5yFvAiYU3qwmXXMh3lDt2QaFAr43Tik3ybHhooF3d8WFw97loxWyn9C4Bg0XOJecsrHhAAoHv5IJAcKms5y6fMtzrtiWgZDZD'
+	url = 'https://graph.facebook.com/v2.6/me/messages?access_token=' + token
 
 	def permission
 		if params['hub.verify_token'] == 'verify_mako'
@@ -14,20 +16,17 @@ class WebhookController < ApplicationController
 	end
 
 	def receive
-		# puts request.body.read
-		# returnee = 'nope'
-		puts "Hello, logs!"
 		request_body = JSON.parse(request.body.read)
 		messaging_events = request_body['entry'][0]['messaging']
-		# event = request_body['entry'][0]['messaging'][0]
 		for i in 0..(messaging_events.count-1)
-			# returnee = i.to_s
 			event = request_body['entry'][0]['messaging'][i]
 			sender = event['sender']['id']
-			puts sender
 			if event['message'] and event['message']['text']
-				sendTextMessage(sender, "hello")
-				# returnee = 'hello'
+				if event['message']['text'] == "lesson"
+					sendAllLessons(sender)
+				else
+					sendTextMessage(sender, "hello")
+				end
 			end
 		end
 
@@ -37,12 +36,28 @@ class WebhookController < ApplicationController
 	private
 
 		def sendTextMessage(sender, text)
-			token = 'EAADrxm348aEBAIyMoDh1rf3lScB2NCOWWm9IUx9H1AAZB9nWyEFwV5vJ98ejHeJC0Mcpp3DhZCS5yFvAiYU3qwmXXMh3lDt2QaFAr43Tik3ybHhooF3d8WFw97loxWyn9C4Bg0XOJecsrHhAAoHv5IJAcKms5y6fMtzrtiWgZDZD'
-			url = 'https://graph.facebook.com/v2.6/me/messages?access_token=' + token
 			recipientData = {:id => sender}
 			messageData = {:text => text}
 			response = Unirest.post url, 
                         headers:{ "Content-Type" => "application/json" }, 
                         parameters:{ :recipient => recipientData, :message => messageData }.to_json
+		end
+
+		def sendAllLessons(sender)
+			recipientData = {:id => sender}
+			elementsData = [{:title => "K'Nex Wheelbarrow", 
+				:subtitle => "Learn about lever classes and pulley systems!", 
+				:image_url => "https://makokit.herokuapp.com/img/sample/knex.jpg", 
+				:buttons => [{:type => "postback", :title => "Learn This!", :payload => "Learn This!"}]}, 
+				{:title => "Cooking Chemistry", 
+				:subtitle => "Fry your eggs like you know what you're doing!", 
+				:image_url => "https://makokit.herokuapp.com/img/sample/cooking.jpg", 
+				:buttons => [{:type => "postback", :title => "Learn This!", :payload => "Learn This!"}]}]
+			payloadData = {:template_type => "generic", :elements => elementsData}
+			messageData = {:type => "template", :payload => payloadData}
+			attachmentData = {:attachment => messageData}
+			response = Unirest.post url, 
+                        headers:{ "Content-Type" => "application/json" }, 
+                        parameters:{ :recipient => recipientData, :message => attachmentData }.to_json
 		end
 end
