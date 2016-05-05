4acab47c7494e9a027ad98c3c96d6adac787a931
Muhammad Khadafi <mk2333@cornell.edu>
2016-05-05 06:44:26 -0400

    whatever

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -46,11 +46,11 @@ class WebhookController < ApplicationController
 				# 	answers = [{:text => "Next", :payload => instruction.next_instruction_id.to_s}]
 				# end
 				answers = [{:text => "Next", :payload => instruction.next_instruction_id.to_s}]
-				if instruction.image_url
+				if instruction.image_url.empty?
 					sendButtons(sender, instruction.text, answers)
 					# sendImageButtons(sender, instruction.text, instruction.image_url, answers)
 				else
-					sendButtons(sender, instruction.text, answers)
+					sendButtons(sender, "not empty", answers)
 				end
 			end
 		end
