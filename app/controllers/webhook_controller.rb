a985e448b5dfd1ad1833d07858b7a64b0fdee448
Muhammad Khadafi <mk2333@cornell.edu>
2016-05-05 16:51:06 -0400

    get first name

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -61,8 +61,6 @@ class WebhookController < ApplicationController
 				unless instruction.image_url.empty?
 					sendImage(sender, instruction.image_url)
 				end
-				text = instruction.text
-				text.gsub! '<name>', "username"
 				sendButtons(sender, text, answers)
 			end
 		end
@@ -96,6 +94,11 @@ class WebhookController < ApplicationController
 		def sendButtons(sender, text, answers)
 			token = 'EAADrxm348aEBAIyMoDh1rf3lScB2NCOWWm9IUx9H1AAZB9nWyEFwV5vJ98ejHeJC0Mcpp3DhZCS5yFvAiYU3qwmXXMh3lDt2QaFAr43Tik3ybHhooF3d8WFw97loxWyn9C4Bg0XOJecsrHhAAoHv5IJAcKms5y6fMtzrtiWgZDZD'
 			url = 'https://graph.facebook.com/v2.6/me/messages?access_token=' + token
+
+			if text.include? "<name>":
+				username = getUserName(sender)
+				text.gsub! "<name>", username
+			end
 
 			buttons = []
 			answers.each do |a|
