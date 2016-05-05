cc69fbcb9483f3696ced0db39055bbd0ac5df722
Muhammad Khadafi <mk2333@cornell.edu>
2016-05-05 11:05:44 -0400

    fuzzy matching

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -2,6 +2,7 @@ class WebhookController < ApplicationController
 
 	require 'unirest'
 	require 'json'
+	require 'fuzzystringmatch'
 
 	skip_before_action :verify_authenticity_token
 
@@ -21,10 +22,15 @@ class WebhookController < ApplicationController
 			event = request_body['entry'][0]['messaging'][i]
 			sender = event['sender']['id']
 			if event['message'] and event['message']['text']
-				if event['message']['text'] == "lesson"
+				if event['message']['text'] == "lesson" or event['message']['text'] == "Lesson"
+					sendTextMessage(sender, "Here are our most recent lessons")
 					sendAllLessons(sender)
+				elsif  event['message']['text'] == "help"
+					help = "Tell Mako what you want to learn. Or type 'lesson' to see most recent lessons"
+					sendTextMessage(sender, help)
 				else
-					sendTextMessage(sender, "hello")
+					sendTextMessage(sender, "Here are the lessons that matched '" + event['message']['text'] + "'")
+					sendLessons(sender, event['message']['text'])
 				end
 			end
 			if event['postback'] and event['postback']['payload']
@@ -67,6 +73,8 @@ class WebhookController < ApplicationController
                         headers:{ "Content-Type" => "application/json" }, 
                         parameters:{ :recipient => recipientData, :message => messageData }.to_json
 		end
+		jarow = FuzzyStringMatch::JaroWinkler.create( :native )
+p jarow.getDistance(  "jones",      "johnson" )
 
 		def sendImage(sender, image_url)
 			token = 'EAADrxm348aEBAIyMoDh1rf3lScB2NCOWWm9IUx9H1AAZB9nWyEFwV5vJ98ejHeJC0Mcpp3DhZCS5yFvAiYU3qwmXXMh3lDt2QaFAr43Tik3ybHhooF3d8WFw97loxWyn9C4Bg0XOJecsrHhAAoHv5IJAcKms5y6fMtzrtiWgZDZD'
@@ -98,11 +106,41 @@ class WebhookController < ApplicationController
                         parameters:{ :recipient => recipientData, :message => attachmentData }.to_json
 		end
 
+		def sendLessons(sender, lessonText)
+			token = 'EAADrxm348aEBAIyMoDh1rf3lScB2NCOWWm9IUx9H1AAZB9nWyEFwV5vJ98ejHeJC0Mcpp3DhZCS5yFvAiYU3qwmXXMh3lDt2QaFAr43Tik3ybHhooF3d8WFw97loxWyn9C4Bg0XOJecsrHhAAoHv5IJAcKms5y6fMtzrtiWgZDZD'
+			url = 'https://graph.facebook.com/v2.6/me/messages?access_token=' + token
+
+			jarow = FuzzyStringMatch::JaroWinkler.create( :native )
+			# jarow.getDistance("jones", "johnson" )
+			lessons = Lesson.order(id: :desc)
+			elementsData = []
+			lessons.each do |l|
+				next if l.image_url == '' or jarow.getDistance(l.name, lessonText) < 0.6
+
+				instruction = Instruction.where(lesson_id: l.id).order(:display_index).take
+				# payload = "text:#{instruction.text},,,next:#{instruction.next_instruction_id},,,image_url:#{instruction.image_url}"
+				lessonData = {:title => l.name, 
+				:subtitle => l.description, 
+				:image_url => l.image_url, 
+				:buttons => [{:type => "postback", :title => "Learn This!", :payload => instruction.id.to_s}]}
+
+				elementsData.push(lessonData)
+			end
+
+			recipientData = {:id => sender}
+			payloadData = {:template_type => "generic", :elements => elementsData}
+			messageData = {:type => "template", :payload => payloadData}
+			attachmentData = {:attachment => messageData}
+			response = Unirest.post url, 
+                        headers:{ "Content-Type" => "application/json" }, 
+                        parameters:{ :recipient => recipientData, :message => attachmentData }.to_json
+		end
+
 		def sendAllLessons(sender)
 			token = 'EAADrxm348aEBAIyMoDh1rf3lScB2NCOWWm9IUx9H1AAZB9nWyEFwV5vJ98ejHeJC0Mcpp3DhZCS5yFvAiYU3qwmXXMh3lDt2QaFAr43Tik3ybHhooF3d8WFw97loxWyn9C4Bg0XOJecsrHhAAoHv5IJAcKms5y6fMtzrtiWgZDZD'
 			url = 'https://graph.facebook.com/v2.6/me/messages?access_token=' + token
 
-			lessons = Lesson.order(:id)
+			lessons = Lesson.order(id: :desc)
 			elementsData = []
 			lessons.each do |l|
 				next if l.image_url == ''
