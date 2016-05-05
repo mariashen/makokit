405005484229e85120ca46af7a52e430f73f015a
Muhammad Khadafi <mk2333@cornell.edu>
2016-05-05 04:34:49 -0400

    whatever

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -58,13 +58,12 @@ class WebhookController < ApplicationController
 			lessons.each do |l|
 				next if l.image_url == ''
 
-				instruction = Instruction.order(display_index: :asc).find_by lesson_id: l.id
-				puts instruction
+				instruction = Instruction.where(lesson_id: 3).order(:display_index).take
 
 				lessonData = {:title => l.name, 
 				:subtitle => l.description, 
 				:image_url => l.image_url, 
-				:buttons => [{:type => "postback", :title => "Learn This!", :payload => "instruction.text"}]}
+				:buttons => [{:type => "postback", :title => "Learn This!", :payload => instruction.text}]}
 
 				elementsData.push(lessonData)
 			end
