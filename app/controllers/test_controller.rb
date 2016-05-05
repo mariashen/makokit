fa88bff8ff7bcfb92cdb743ebf4d6f60dc9628fa
Muhammad Khadafi <mk2333@cornell.edu>
2016-05-05 04:17:03 -0400

    whatever

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -1,7 +1,7 @@
 class TestController < ApplicationController
 	def test
 
-		instruction = Instruction.order(display_index: :asc).find_by lesson_id: l.id
-		render plain: instruction
+		# instruction = Instruction.order(display_index: :asc).find_by lesson_id: l.id
+		render plain: 'instruction'
 	end
 end
